54c47dc4ca75a1dc8232d196f7bcf75ae2941ba5
Nksama <75163978+Nksama@users.noreply.github.com>
2021-04-21 16:15:50 +0530

    Update telegraph.py

--- YoneRobot/modules/telegraph.py
+++ YoneRobot/modules/telegraph.py
@@ -17,7 +17,7 @@ TMP_DOWNLOAD_DIRECTORY = "./"
 BOTLOG = False
 
 
-@tbot.on(events.NewMessage(pattern="/telegraph (media|text) ?(.*)"))
+@register(events.NewMessage(pattern="/telegraph (media|text) ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return
