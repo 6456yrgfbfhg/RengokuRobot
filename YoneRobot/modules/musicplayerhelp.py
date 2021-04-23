49ac1bd0c78c57c4eb86f9a9541c66a038ac2c7d
Nksama <75163978+Nksama@users.noreply.github.com>
2021-04-23 14:02:39 +0530

    Create musicplayerhelp.py

---
+++ YoneRobot/modules/musicplayerhelp.py
@@ -0,0 +1,48 @@
+__help__ = """
+
+Help for Music Player module:
+
+          》** DAISYXMUSIC v2 ** 《
+            
+ DAISYXMUSIC plays plays music in your group's voice chat
+ 
+ Assistant name >> @munsic_player
+ 
+ Setting up 
+
+1) Make bot admin
+2) Start a voice chat
+3) Try `/play [song name]` for the first time by an admin
+*) If userbot joined enjoy music, If not add @DaisyXhelper to your group and retry
+
+ Commands 
+
+=>> Song Playing 🎧 
+
+  /play: Play song using youtube music
+  /play [yt url] : Play the given yt url
+  /dplay: Play song via deezer
+  /splay: Play song via jio saavn
+  
+=>> Playback ⏯
+
+  /player: Open Settings menu of player
+  /skip: Skips the current track
+  /pause: Pause track
+  /resume: Resumes the paused track
+  /end: Stops media playback
+  /current: Shows the current Playing track
+  /playlist: Shows playlist
+
+=>> More tools 🧑‍🔧
+
+  /admincache: Updates admin info of your group. Try if bot isn't recognize admin
+  /userbotjoin: Invite @DaisyXhelper Userbot to your chat
+
+*Player cmd and all other cmds except /play, /current  and /playlist  are only for admins with manage group
+
+ PLEASE NOTE THIS SERVICE IS UNSTABLE AND CAN BE STOPPED ANYTIME
+
+"""
+
+_mod_name_ = "Music player"
