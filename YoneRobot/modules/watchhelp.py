cde3c2e30dba20638edb8cc6b75ff497a95a4046
Nksama <75163978+Nksama@users.noreply.github.com>
2021-04-24 21:01:23 +0530

    Update watchhelp.py

--- YoneRobot/modules/watchhelp.py
+++ YoneRobot/modules/watchhelp.py
@@ -2,5 +2,10 @@ __mod_name__ = "Watch"
 
 __help__ = """
  ❍ /watch <anime name> - get anime download links
+
 Note: disable adblocker and solve the CAPTCHA
+
+❍ /watchorder <anime name> - to get anime watchorder
+(without <>)
+
 """
