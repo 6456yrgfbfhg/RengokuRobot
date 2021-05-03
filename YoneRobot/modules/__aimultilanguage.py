4c1f03510f2ebf6cfe76d5cd33426c9c2100ab95
Nksama <75163978+Nksama@users.noreply.github.com>
2021-05-03 12:11:49 +0530

    Update __aimultilanguage.py

--- YoneRobot/modules/__aimultilanguage.py
+++ YoneRobot/modules/__aimultilanguage.py
@@ -67,7 +67,7 @@ async def hmm(_, message):
         lel = await edit_or_reply(message, "`Processing...`")
         Escobar = remove_chat(int(message.chat.id))
         if not Escobar:
-            await lel.edit("yone AI Was Not Activated In This Chat")
+            await lel.edit("rengoku AI Was Not Activated In This Chat")
             return
         await lel.edit(
             f"rengoku AI Successfully Deactivated For Users In The Chat {message.chat.id}"
@@ -233,8 +233,8 @@ async def inuka(client, message):
     # test = emoji.demojize(test.strip())
 
     # Kang with the credits bitches @InukaASiTH
-    test = test.replace("yone", "Aco")
-    test = test.replace("yone", "Aco")
+    test = test.replace("rengoku", "Aco")
+    test = test.replace("rengoku", "Aco")
     querystring = {
         "bid": "178",
         "key": "sX5A2PcYZbsN5EY6",
@@ -336,119 +336,4 @@ async def inuka(client, message):
         await yone.send_chat_action(message.chat.id, "typing")
         await message.reply_text(pro)
     except CFError as e:
-        print(e)sage.sender_id,
-        )
-    )
-    p = result.participant
-    return isinstance(p, types.ChannelParticipantCreator) or (
-        isinstance(p, types.ChannelParticipantAdmin) and p.admin_rights.change_info
-    )
-
-
-@register(pattern="^/yeschat$")
-async def _(event):
-    if event.is_group:
-        if not event.sender_id == OWNER_ID:
-           if not await can_change_info(message=event):
-              return
-    else:
-        return
-    chat = event.chat
-    is_chat = sql.is_chat(chat.id)
-    k = ly.is_chat(chat.id)
-    if k:
-        ly.rem_chat(chat.id)
-    if not is_chat:
-        ses_id = 'null'
-        expires = 'null'
-        sql.set_ses(chat.id, ses_id, expires)
-        await event.reply("AI successfully enabled for this chat!")
-        return
-    await event.reply("AI Bot is already enabled for this chat!")
-    return ""
-
-
-@register(pattern="^/nochat$")
-async def _(event):
-    if event.is_group:
-        if not event.sender_id == OWNER_ID:
-          return
-    else:
-        return
-    chat = event.chat
-    is_chat = sql.is_chat(chat.id)
-    if not is_chat:
-        await event.reply("AI isn't enabled here in the first place!")
-        return
-    sql.rem_chat(chat.id)
-    await event.reply("AI Bot disabled successfully!")
-
-
-@tbot.on(events.NewMessage(pattern=None))
-async def _(event):
-  if event.is_group:
-        pass
-  else:
-        return
-  prof = str(event.text)
-  
-  if not "Yone" in prof:
-    if not "yone" in prof:
-      reply_msg = await event.get_reply_message()
-      if reply_msg.sender_id == BOT_ID:
-           return
-      else:
-          return
-  chat = event.chat
-  msg = prof
-  is_chat = sql.is_chat(chat.id)
-  if not is_chat:
-        return
-  if msg.startswith("/") or msg.startswith("@"):
-    return
-  lan = translator.detect(msg)
-  if not "en" in lan and not lan == "":
-     test = translator.translate(msg, lang_tgt="en")
-  else:
-     test = msg
-  
-  url = "https://iamai.p.rapidapi.com/ask"
-  r = ('\n    \"consent\": true,\n    \"ip\": \"::1\",\n    \"question\": \"{}\"\n').format(test)
-  k = f"({r})"
-  new_string = k.replace("(", "{")
-  lol = new_string.replace(")","}")
-  payload = lol
-  headers = {
-    'content-type': "application/json",
-    'x-forwarded-for': "<user's ip>",
-    'x-rapidapi-key': "33b8b1a671msh1c579ad878d8881p173811jsn6e5d3337e4fc",
-    'x-rapidapi-host': "iamai.p.rapidapi.com"
-    }
-
-  response = requests.request("POST", url, data=payload, headers=headers)
-  lodu = response.json()
-  result = (lodu['message']['text'])
-  if "Thergiakis" in result:
-   pro = random.choice(string)
-   try:
-      async with tbot.action(event.chat_id, 'typing'):
-           await event.reply(pro)
-   except CFError as e:
-           print(e)
-  elif "Ann" in result:
-   pro = "Yeah, My name is Rengoku"
-   try:
-      async with tbot.action(event.chat_id, 'typing'):
-           await event.reply(pro)
-   except CFError as e:
-           print(e)
-  else:
-    if not "en" in lan and not lan == "":
-      finale = translator.translate(result, lang_tgt=lan[0])
-    else:
-      finale = result
-    try:
-      async with tbot.action(event.chat_id, 'typing'):
-           await event.reply(finale)
-    except CFError as e:
-           await event.reply(lodu)
+        print(e)
